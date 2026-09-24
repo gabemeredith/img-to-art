21fcf79132ec12b85db81d718a5fc78983f3f7f0
Gabe Meredith <gabriel.b.meredith@gmail.com>
2026-09-24 13:11:23 -0400

    terminal done, to be added is dynamic terminal sizing and web features

--- main.py
+++ main.py
@@ -15,7 +15,30 @@ def image_to_unicode(image_path,new_width=100):
     aspect_ratio = original_height / original_width
     new_height = int(new_width * (aspect_ratio) * VERTICAL_CORRECTION)
     
-    img_resized = image.resize((new_height,new_width))
-    grayscale = list(img_resized.convert("L"))
+    img_resized = image.resize((new_width,new_height))
+    grayscale = list(img_resized.convert("L").getdata())
+    #converting to the Unicode shades
+    unicode_list = []
+    for pixel in grayscale: 
+        index = pixel // 51
+        if index > 4:
+            index = 4
+        unicode_list.append(UNICODE_SHADES[index])
     
+    rows = []
+    #convert to string 
+    for i in range(0,len(unicode_list),new_width):
+        row_s = "".join(unicode_list[i:i + new_width])
+        
+        rows.append(row_s)
+    final_ascii_art = "\n".join(rows)
+    return final_ascii_art
+    
+    
+if __name__ == "__main__":
+    test_img = "funny.jpg"
+    
+    result = image_to_unicode(test_img, new_width=150)
+    
+    print(result)
     
